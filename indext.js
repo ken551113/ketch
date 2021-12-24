e1038baa20c749457d2d35d472b5867f8f47bbe0
Ken Lee <ken.lee@kangdainfo.com>
2021-12-24 09:46:43 +0800

    初步封裝

--- indext.js
+++ indext.js
@@ -1,3 +1,33 @@
-import ketch from "./lib/ketch";
+import "regenerator-runtime/runtime";
 
-ketch();
+import Fetch from "./lib/index.js";
+
+const fetch = new Fetch();
+
+const fetchWithBaseUrl = new Fetch({
+  BASE_URL: "https://jsonplaceholder.typicode.com",
+});
+
+fetch
+  .get({
+    url: "https://jsonplaceholder.typicode.com/comments",
+    query: "postId=1",
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+fetchWithBaseUrl
+  .get({
+    url: "comments",
+    query: "postId=1",
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
